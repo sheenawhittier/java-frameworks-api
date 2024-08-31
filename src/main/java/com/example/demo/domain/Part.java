301301e70243023e340463f599bb3d657bd9454e
Sheena Whittier <swh1309@wgu.edu>
2024-08-30 20:12:55 -0600

    Added ValidInv import to Part.java

--- src/main/java/com/example/demo/domain/Part.java
+++ src/main/java/com/example/demo/domain/Part.java
@@ -1,5 +1,6 @@
 package com.example.demo.domain;
 
+import com.example.demo.validators.ValidInv;
 import com.example.demo.validators.ValidMinMax;
 
 import javax.persistence.*;
@@ -13,6 +14,7 @@ import java.util.Set;
 @DiscriminatorColumn(name = "part_type", discriminatorType = DiscriminatorType.INTEGER)
 @Table(name = "Parts")
 @ValidMinMax
+@ValidInv
 public abstract class Part implements Serializable {
 
     @Id
